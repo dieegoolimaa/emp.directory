0731f815e8e2978d710e7a24929e8ccca9992689
Celso Gando <Celsogando7@gmail.com>
2024-04-15 17:19:26 +0200

    About done

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,55 +1,56 @@
 import "./component-style/about.css";
+
 function About() {
   return (
+    <div>
       <div className="about">
-          <h1>About</h1>
-          <h2>Description</h2>
+        <h1>About</h1>
+        <h2>(TIED)</h2>
+        <p>Tech Innovators Employee Directory </p>
 
-<h3>Introduction to the Company:
-#Tech Innovators Employee Directory (TIED)
+        <h3>History</h3>
+        Founded in the beginning of 2021 by visionary entrepreneurs, Tech Innovators started as a small software development firm.
+        Over the years, we’re growing into a global technology powerhouse, serving Fortune 500 companies across diverse industries.
 
-#History
-Founded in # by visionary entrepreneurs, Tech Innovators started as a small software development firm.
-Over the years, we’ve grown into a global technology powerhouse, serving Fortune 500 companies across diverse industries.
+        <h3>Company Overview</h3>
+        Mission: At Tech Innovators, our mission is to empower businesses through cutting-edge technology solutions.
+        We believe in driving innovation, fostering collaboration, and delivering exceptional value to our clients.
 
-Company Overview
-Mission: At Tech Innovators, our mission is to empower businesses through cutting-edge technology solutions.
-We believe in driving innovation, fostering collaboration, and delivering exceptional value to our clients.
-
-#Values:
-Innovation: We thrive on creativity and embrace new ideas.
-Integrity: We uphold honesty, transparency, and ethical conduct.
-Teamwork: Together, we achieve more than we can individually.</h3>
-
-
-<h3>Meet the Team:</h3>
-#Diego Lima
-FrontEnd Developer
-Email: #dieegoolimaa@gmail.com
-LinkedIn: #
-Github: https://github.com/dieegoolimaa
-Image: 
-
-
-#Celso Gando
-Webdev
-Email: MrGandomaestro@outlook.com
-LinkedIn: #
-GitHub: https://github.com/TheOne-1-dot
-Image: 
-
-
-#Rui Folgado
-Full Stack Developer
-Email: rui.folgado@hotmail.com
-LinkedIn: #
-GitHub: https://github.com/Folgad0
-Image:
-
-
-Email: 
-
+        <h3>Values:</h3>
+        <p>Innovation: We thrive on creativity and embrace new ideas.</p>
+        <p>Integrity: We uphold honesty, transparency, and ethical conduct.</p>
+        <p>Teamwork: Together, we achieve more than we can individually.</p>
       </div>
+
+      <div className="container">
+        <div className="column">
+          <h2>Diego Lima</h2>
+          <p>Role: FrontEnd Developer</p>
+          <p>Email: dieegoolimaa@gmail.com</p>
+          <p>LinkedIn: #</p>
+          <p>Github: https://github.com/dieegoolimaa</p>
+          <img src="Public/Images/Diego.jpg" alt="Diego Lima" />
+        </div>
+
+        <div className="column">
+          <h2>Celso Gando</h2>
+          <p>Role: FrontEnd Dev</p>
+          <p>Email: MrGandomaestro@outlook.com</p>
+          <p>LinkedIn: www.linkedin.com/in/celso-gando-301b072b1</p>
+          <p>GitHub: https://github.com/TheOne-1-dot</p>
+          <img src="Public/Images/Celso.jpg" alt="Celso Gando" />
+        </div>
+
+        <div className="column">
+          <h2>Rui Folgado</h2>
+          <p>Role: Full Stack Developer</p>
+          <p>Email: rui.folgado@hotmail.com</p>
+          <p>LinkedIn: #</p>
+          <p>GitHub: https://github.com/Folgad0</p>
+          <img src="Public/Images/Rui.jpg" alt="Rui Folgado" />
+        </div>
+      </div>
+    </div>
   );
 }
 
