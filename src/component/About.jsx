8f6a85365a478bba7635a2b8a28d70a73f87cfb6
dieegoolimaa <dieegoolimaa@gmail.com>
2024-04-16 11:54:38 +0100

    add about style

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,4 +1,7 @@
 import "./component-style/about.css";
+import { MdEmail, MdHeight } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithubSquare, FaUser } from "react-icons/fa";
 
 function About() {
   return (
@@ -17,96 +20,90 @@ function About() {
         companies across diverse industries.
         <h3>
           {" "}
+          <br />
+          <br />
           <strong>Company Overview:</strong>{" "}
         </h3>
         <strong> Mission </strong> At Tech Innovators, our mission is to empower
         businesses through cutting-edge technology solutions. We believe in
         driving innovation, fostering collaboration, and delivering exceptional
         value to our clients.
+        <br />
+        <br />
         <h3>
           {" "}
-          <strong>Values</strong>{" "}
+          <strong>Values</strong>
         </h3>
         <p>
           {" "}
-          <strong>Innovation</strong>: We thrive on creativity and embrace new
-          ideas.
+          <strong>Innovation</strong>: <br />
+          We thrive on creativity and embrace new ideas.
         </p>
         <p>
           {" "}
-          <strong>Integrity</strong>: We uphold honesty, transparency, and
-          ethical conduct.
+          <strong>Integrity</strong>: <br />
+          We uphold honesty, transparency, and ethical conduct.
         </p>
         <p>
           {" "}
-          <strong>Teamwork</strong>: Together, we achieve more than we can
-          individually.
+          <strong>Teamwork</strong>: <br />
+          Together, we achieve more than we can individually.
         </p>
       </div>
 
       <div className="container">
         <div className="column">
-          <h2> Diego Lima</h2>
+          <img src="Public/Images/Diego.jpg" alt="Diego Fernandes" />
+          <h2>Diego Fernandes</h2>
+          <p>Frontend Developer</p>
           <p>
-            {" "}
-            <strong>Role: </strong>FrontEnd Developer
+            <MdEmail /> <a href="mailto:dieegoolimaa@gmail.com">Email</a>
           </p>
           <p>
-            {" "}
-            <strong>Email: </strong> dieegoolimaa@gmail.com
+            <FaLinkedin />{" "}
+            <a href="https://www.linkedin.com/in/diego-fernandes-20/">
+              LinkedIn
+            </a>
           </p>
           <p>
-            {" "}
-            <strong>LinkedIn: </strong>#
+            <FaGithubSquare />{" "}
+            <a href="https://github.com/dieegoolimaa">GitHub</a>
           </p>
-          <p>
-            {" "}
-            <strong>Github: </strong> https://github.com/dieegoolimaa
-          </p>
-          <img src="Public/Images/Diego.jpg" alt="Diego Lima" />
         </div>
 
         <div className="column">
-          <h2>Celso Gando</h2>
-          <p>
-            {" "}
-            <strong> Role:</strong> FrontEnd Dev
-          </p>
-          <p>
-            {" "}
-            <strong>Email: </strong> MrGandomaestro@outlook.com
-          </p>
-          <p>
-            {" "}
-            <strong> LinkedIn: </strong>{" "}
-            www.linkedin.com/in/celso-gando-301b072b1
-          </p>
-          <p>
-            {" "}
-            <strong> GitHub: </strong> https://github.com/TheOne-1-dot
-          </p>
           <img src="Public/Images/Celso.jpg" alt="Celso Gando" />
+          <h2>Celso Gando</h2>
+          <p>Frontend Dev</p>
+          <p>
+            <MdEmail /> <a href="mailto:MrGandomaestro@outlook.com">Email</a>
+          </p>
+          <p>
+            <FaLinkedin />{" "}
+            <a href="https://www.linkedin.com/in/celso-gando-301b072b1/">
+              LinkedIn
+            </a>
+          </p>
+          <p>
+            <FaGithubSquare />{" "}
+            <a href="https://github.com/TheOne-1-dot">GitHub</a>
+          </p>
         </div>
 
         <div className="column">
-          <h2>Rui Folgado</h2>
-          <p>
-            {" "}
-            <strong>Role: </strong> Full Stack Developer
-          </p>
-          <p>
-            {" "}
-            <strong>Email: </strong> rui.folgado@hotmail.com
-          </p>
-          <p>
-            {" "}
-            <strong>LinkedIn: </strong> #
-          </p>
-          <p>
-            {" "}
-            <strong>GitHub: </strong> https://github.com/Folgad0
-          </p>
           <img src="Public/Images/Rui.jpg" alt="Rui Folgado" />
+          <h2>Rui Folgado</h2>
+          <p>Full Stack Developer</p>
+          <p>
+            <MdEmail /> <a href="mailto:rui.folgado@hotmail.com">Email</a>
+          </p>
+          <p>
+            <FaLinkedin /> Not available
+          </p>
+          <p>
+            <FaGithubSquare />
+            <a href="https://github.com/Folgad0"> GitHub</a>
+          </p>
         </div>
       </div>
     </div>
