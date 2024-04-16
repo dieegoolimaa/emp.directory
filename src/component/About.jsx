f1904f1ebdb02328119298487124748149f07138
Celso Gando <Celsogando7@gmail.com>
2024-04-16 11:03:22 +0200

    Text highligh in about

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -8,18 +8,18 @@ function About() {
         <h2>(TIED)</h2>
         <p>Tech Innovators Employee Directory </p>
 
-        <h3>History</h3>
+        <h3> <strong> History </strong> </h3>
         Founded in the beginning of 2021 by visionary entrepreneurs, Tech Innovators started as a small software development firm.
         Over the years, we’re growing into a global technology powerhouse, serving Fortune 500 companies across diverse industries.
 
-        <h3>Company Overview</h3>
-        Mission: At Tech Innovators, our mission is to empower businesses through cutting-edge technology solutions.
+        <h3> <strong> Company Overview </strong> </h3>
+        <strong> Mission </strong>: At Tech Innovators, our mission is to empower businesses through cutting-edge technology solutions.
         We believe in driving innovation, fostering collaboration, and delivering exceptional value to our clients.
 
-        <h3>Values:</h3>
-        <p>Innovation: We thrive on creativity and embrace new ideas.</p>
-        <p>Integrity: We uphold honesty, transparency, and ethical conduct.</p>
-        <p>Teamwork: Together, we achieve more than we can individually.</p>
+        <h3><strong>Values</strong>: </h3>
+        <p><strong> Innovation </strong>: We thrive on creativity and embrace new ideas.</p>
+        <p><strong> Integrity </strong>: We uphold honesty, transparency, and ethical conduct.</p>
+        <p><strong> Teamwork </strong>: Together, we achieve more than we can individually.</p>
       </div>
 
       <div className="container">
